22e1f88a95817637694aeb6ecd384db3282897d8
YossefMagdy <y.f.magdy@gmail.com>
2023-07-01 17:46:52 +0300

    Edit

--- src/app/Core/Service/employe.service.ts
+++ src/app/Core/Service/employe.service.ts
@@ -8,15 +8,15 @@ export class EmployeService {
   position:number=0
   Id:number=99
   ELEMENT_DATA: EmployeeList[] = [
-    { Position: 0, Status: 'فعال', Name: 'محمد', UserName: 100, UserType: 'حامل الكارت', Job: 'مدير', MobilNumber: 23424 },
-    { Position: 0, Status: 'غير فعال', Name: 'يوسف', UserName: 100, UserType: 'مدير الهيكل التنظيمي', Job: 'محاسب', MobilNumber: 23424 },
-    { Position: 0, Status: 'فعال', Name: 'اسلام', UserName: 100, UserType: 'حامل الكارت', Job: 'محاسب', MobilNumber: 23424 },
-    { Position: 0, Status: 'فعال', Name: 'مصطفي', UserName: 100, UserType: 'مدير الهيكل التنظيمي', Job: 'مدير', MobilNumber: 23424 },
+    { Position: 0, Status: 'فعال', Name: 'محمد', UserName: 100, UserType: 'حامل الكارت', Job: 'مدير', MobilNumber: 1258339252 },
+    { Position: 0, Status: 'غير فعال', Name: 'يوسف', UserName: 100, UserType: 'مدير الهيكل التنظيمي', Job: 'محاسب', MobilNumber: 1258339252 },
+    { Position: 0, Status: 'فعال', Name: 'اسلام', UserName: 100, UserType: 'حامل الكارت', Job: 'محاسب', MobilNumber: 1258339252 },
+    { Position: 0, Status: 'فعال', Name: 'مصطفي', UserName: 100, UserType: 'مدير الهيكل التنظيمي', Job: 'مدير', MobilNumber: 1258339252 },
     { Position: 0, Status: 'غير فعال', Name: 'عبدالرحمن ', UserName: 100, UserType: 'حامل الكارت', Job: 'مدير', MobilNumber: 1149929873 },
-    { Position: 0, Status: 'غير فعال', Name: 'احمد', UserName: 100, UserType: 'حامل الكارت', Job: 'محاسب', MobilNumber: 23424 },
-    { Position: 0, Status: 'غير فعال', Name: 'ختام', UserName: 100, UserType: 'مدير الهيكل التنظيمي', Job: 'محاسب', MobilNumber: 23424 },
-    { Position: 0, Status: 'غير فعال', Name: 'ايه', UserName: 100, UserType: 'حامل الكارت', Job: 'مدير', MobilNumber: 23424 },
-    { Position: 0, Status: 'غير فعال', Name: 'هاجر', UserName: 100, UserType: 'مدير الهيكل التنظيمي', Job: 'مدير', MobilNumber: 23424 },
+    { Position: 0, Status: 'غير فعال', Name: 'احمد', UserName: 100, UserType: 'حامل الكارت', Job: 'محاسب', MobilNumber: 1258339252 },
+    { Position: 0, Status: 'غير فعال', Name: 'ختام', UserName: 100, UserType: 'مدير الهيكل التنظيمي', Job: 'محاسب', MobilNumber: 1258339252 },
+    { Position: 0, Status: 'غير فعال', Name: 'احمد', UserName: 100, UserType: 'حامل الكارت', Job: 'مدير', MobilNumber: 1258339252 },
+    { Position: 0, Status: 'غير فعال', Name: 'احمد', UserName: 100, UserType: 'مدير الهيكل التنظيمي', Job: 'مدير', MobilNumber: 1258339252 },
   ];
 
   constructor() { 
